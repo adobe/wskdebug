cc59aac98921d82dbfe12fd6294be5f89502fb62
Alexander Klimetschek <aklimets@adobe.com>
2019-06-14 14:11:48 -0700

    changing deploy.sh to open web action in chrome for working live reload

--- examples/deploy.sh
+++ examples/deploy.sh
@@ -5,4 +5,4 @@ PKG=wskdebug-examples
 wsk package update $PKG
 wsk action update $PKG/webaction webaction.js --web true
 url=$(wsk action get $PKG/webaction --url | tail -n1)
-open $url
+open -a "Google Chrome" $url
